cf57a0a0322268da95c06edc55a9020451420bb6
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-03 23:24:53 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view id="app"/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +9,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div>
+      <div class="mt-48 justify-center flex animate-bounce">
+        <img class="animate-bounce content-center object-center lg:max-w-xl lg:max-h-xl ..." alt="AirTag" src="../assets/shopping.png">
+      </div>
+      <h1 class="xl:text-9xl text-7xl text-black antialiased font-bold 2xl:resize">
+        AirTags
+      </h1>
+      <div class="mt-3.5 justify-center flex">
+        <button
+            class="px-9 py-4 font-semibold rounded-md rounded-sm block sm:w-auto border-purple-300 bg-purple-200 hover:bg-purple-300 text-purple-700">
+          <a href="https://www.apple.com/shop/buy-airtag/airtag">buy one at the Apple Website</a>
+        </button>
+      </div>
+    </div>
+    <Footer site-name="Home"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Footer
+  }
+}
+</script>
