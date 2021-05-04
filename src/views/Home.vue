cde3429d7a67595a57e7eb7047fc0565e9ab2d51
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-04 23:07:19 +0200

    fixing some bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,7 @@
             class="px-9 py-4 font-semibold rounded-md rounded-sm block sm:w-auto border-purple-300 bg-purple-200 hover:bg-purple-300 text-purple-700">
           <a href="https://www.apple.com/shop/buy-airtag/airtag">buy one at the Apple Website</a>
         </button>
+
       </div>
     </div>
     <Footer site-name="Home"/>
